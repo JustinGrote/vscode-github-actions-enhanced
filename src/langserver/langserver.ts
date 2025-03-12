904e8a5f24b3f0f0e20d13ec2005a531135bc219
Justin Grote <JustinGrote@users.noreply.github.com>
2025-03-11 17:30:55 -0700

    Replace Webpack with esbuild script.
    - Results in bundles half the size and faster build.
    - Automatic watching for launch configurations
    - Improved VSCode tooling for tasks and launch
    - Fix NPM CI to cache correctly

--- src/langserver/langserver.ts
+++ src/langserver/langserver.ts
@@ -1,3 +1,3 @@
 // This file is the main entry point for bundling the language server. It has only a side-effect import of the
-// @actions/languageserver module, so that we can more easily reference it in the webpack configuration.
+// @actions/languageserver module, so that we can more easily reference it in the build configuration.
 import "@actions/languageserver";
